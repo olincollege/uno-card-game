ee1a7292fd8aa888c37cdd9e37a31a93b7c07ce3
Raul Frias Perez <rfriasperez@olin.edu>
2021-04-23 15:32:29 -0400

    Added a uno-game-test to test code

--- uno_deck.py
+++ uno_deck.py
@@ -1,30 +1,29 @@
 class Card:
     
-    def __init__(self, suit, rank):
+    def __init__(self, clr, rank):
         """
         Args:
             suit: A string representing the suit.
             rank: An int represnting the rank of the card (11-13 is J, Q, K).
         """
-        self.suit = suit
+        self.clr = clr
         self.rank = rank
     
     def __repr__(self):
-        if self.rank == 1:
-            rank = "Ace"
-            
+        #Draw Two = 10, Reverse=11, Skip=12
+        #We still have think about the wild and wild draw 4
         #Solution to Question 1 (below) goes here
+        if self.rank == 10:
+            rank = "Draw Two"
         elif self.rank == 11:
-            rank = "Jack"
+            rank = "Reverse"
         elif self.rank == 12:
-            rank = "Queen"
-        elif self.rank == 13:
-            rank = "King"
+            rank = "Skip"
         else:
             rank = self.rank
         #End Solutions
            
-        return f"{rank} of {self.suit}"
+        return f"{rank} of {self.clr}"
 
 import random
 
@@ -32,9 +31,9 @@ class Deck:
     
     def __init__(self):
         self.cards = []
-        for suit in ["Clubs", "Diamonds", "Hearts", "Spades"]:
-            for rank in range(1,14):
-                self.cards.append(Card(suit,rank))
+        for clr in ["Red", "Blue", "Green", "Yellow"]:
+            for rank in range(0,13):
+                self.cards.append(Card(clr,rank))
 
     def shuffle(self):
         random.shuffle(self.cards)
